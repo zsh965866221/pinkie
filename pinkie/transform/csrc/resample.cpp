d7475fae07c765bbe0f588e32262f1e5062b3b68
shuheng.zhang <shuheng.zhang01@united-imaging.com>
2020-10-20 18:13:04 +0800

    resample trilinear

--- pinkie/transform/csrc/resample.cpp
+++ pinkie/transform/csrc/resample.cpp
@@ -4,11 +4,34 @@ namespace pinkie {
 namespace transform {
 
 Image resample_trilinear(
-  const Image& image, 
-  const Frame& frame,
-  const torch::Tensor& size
+  const Image& src_image, 
+  const Frame& dst_frame,
+  const torch::Tensor& dst_size
 ) {
-  return image;
+  auto data = src_image.data();
+  auto src_frame = src_image.frame();
+  auto src_axes = src_frame.axes();
+  auto src_spacing = src_frame.spacing();
+
+  auto delta_x = torch::mm(src_axes, dst_frame.axis(0));
+  delta_x *= (dst_frame.spacing()[0] / src_spacing);
+  auto delta_y = torch::mm(src_axes, dst_frame.axis(1));
+  delta_y *= (dst_frame.spacing()[1] / src_spacing);
+  auto delta_z = torch::mm(src_axes, dst_frame.axis(2));
+  delta_z *= (dst_frame.spacing()[2] / src_spacing);
+
+  auto origin = torch::mm(src_axes, dst_frame.origin()) / src_spacing
+
+  for (int z = 0; z < dst_size[2]; z++) {
+    for (int x = 0; x < dst_size[0]; x++) {
+      for (int y = 0; y < dst_size[1]; y++) {
+        auto curr = origin + delta_z * z + delta_x * x + delta_y * y;
+        // compute trilinear resample
+      }
+    }
+  }
+
+  return src_image;
 }
 
 } // namespace transform
