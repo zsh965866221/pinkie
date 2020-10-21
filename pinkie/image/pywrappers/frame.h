0b65d9d81905a78432164a60ef85db073bd21aa4
shuheng.zhang <shuheng.zhang01@united-imaging.com>
2020-10-21 18:03:36 +0800

    upte

--- pinkie/image/pywrappers/frame.h
+++ pinkie/image/pywrappers/frame.h
@@ -8,11 +8,16 @@ extern "C" {
 void* frame_new();
 void* frame_clone(void* ptr);
 void frame_delete(void* ptr);
+
 void frame_origin(void* ptr, float* out);
 void frame_spacing(void* ptr, float* out);
 void frame_axes(void* ptr, float* out);
 void frame_axis(void* ptr, unsigned int index, float* out);
 
+void frame_set_origin(void* ptr, float* src);
+void frame_set_spacing(void* ptr, float* src);
+void frame_set_axes(void* ptr, float* src);
+void frame_set_axis(void* ptr, unsigned int index, float* src);
 
 #ifdef __cplusplus
 }
